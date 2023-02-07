9579bf2cc79537986cd0dc9c77fb2fa3ae438d79
BryanS1989 <bryanstevenal@gmail.com>
2023-02-07 23:15:03 +0100

    FRONT - Declare onBeforeMount at HomeView

--- frontend-app/src/views/HomeView.vue
+++ frontend-app/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+import { onBeforeMount } from "vue";
+
+
 import HomePage from '../components/HomePage.vue';
+
+onBeforeMount(() => {
+    console.log('[HomeView] [onBeforeMount]');
+});
 </script>
 
 <template>
