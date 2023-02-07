a34e49661494e23d21d6b172fa63d1f866ec78d7
BryanS1989 <bryanstevenal@gmail.com>
2023-02-07 23:15:58 +0100

    FRONT - Declare onBeforeMount at LoginView and use LoginForm component

--- frontend-app/src/views/LoginView.vue
+++ frontend-app/src/views/LoginView.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+
+import LoginForm from "@/components/login/LoginForm.vue";
+
+onBeforeMount(() => {
+    console.log('[LoginView] [onBeforeMount]');
+});
+</script>
+
 <template>
     <h1>Login</h1>
+    <LoginForm/>
 </template>
 
 <style></style>
