1415c8a41d1a17a7e936abeb60cc2cce5e8ef5f0
BryanS1989 <bryanstevenal@gmail.com>
2023-02-07 23:30:53 +0100

    FRONT - Remove unnecessary css rules at App.vue

--- frontend-app/src/App.vue
+++ frontend-app/src/App.vue
@@ -4,28 +4,16 @@ import { RouterLink, RouterView } from 'vue-router';
 
 <template>
     <header>
-        <div class="wrapper">
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/login">login</RouterLink>
-            </nav>
-        </div>
+        <nav>
+            <RouterLink to="/login">login</RouterLink>
+            <RouterLink to="/">Home</RouterLink>
+        </nav>
     </header>
 
     <RouterView />
 </template>
 
 <style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
 nav {
     width: 100%;
     font-size: 12px;
@@ -52,22 +40,6 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
     nav {
         text-align: left;
         margin-left: -1rem;
