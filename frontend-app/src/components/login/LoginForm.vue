f5dd5a88d6176dcadd6ccae6aa737e306082c690
BryanS1989 <bryanstevenal@gmail.com>
2023-02-07 23:15:32 +0100

    FRONT - WIP create basic login form (LoginForm component)

---
+++ frontend-app/src/components/login/LoginForm.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+
+onBeforeMount(() => {
+    console.log('[LoginView] [onBeforeMount]');
+});
+</script>
+
+<template>
+    <form>
+        <input type="email">
+        <input type="password">
+
+        <button type="submit">LOGIN</button>
+    </form>
+</template>
+
+<style></style>
