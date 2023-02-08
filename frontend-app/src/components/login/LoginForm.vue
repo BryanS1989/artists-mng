bbc66d405f157fc4623371919b24ae6a57936ba2
BryanS1989 <bryanstevenal@gmail.com>
2023-02-08 20:27:48 +0100

    FRONT - Modify LoginForm: - Use User interface - Define and emit emitted events - use v-model on inputs

--- frontend-app/src/components/login/LoginForm.vue
+++ frontend-app/src/components/login/LoginForm.vue
@@ -1,5 +1,26 @@
 <script setup lang="ts">
-import { onBeforeMount } from "vue";
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+
+import { onBeforeMount } from 'vue';
+
+import type User from '@/interfaces/users/user.interface';
+
+const emit = defineEmits<{
+    (e: 'attemptLogin', user: User): void;
+}>();
+
+let user: Ref<User> = ref({} as User);
+
+const submitForm = (): void => {
+    console.log(
+        '[LoginForm] [submitForm] email: ',
+        user.value.email,
+        ' password: ',
+        user.value.password
+    );
+    emit('attemptLogin', user.value);
+};
 
 onBeforeMount(() => {
     console.log('[LoginView] [onBeforeMount]');
@@ -7,9 +28,17 @@ onBeforeMount(() => {
 </script>
 
 <template>
-    <form>
-        <input type="email">
-        <input type="password">
+    <form @submit.prevent="submitForm">
+        <input
+            type="email"
+            v-model.trim="user.email"
+            placeholder="Enter your email"
+        />
+        <input
+            type="password"
+            v-model.trim="user.password"
+            placeholder="******"
+        />
 
         <button type="submit">LOGIN</button>
     </form>
