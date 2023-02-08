2279ad784e91cf70d487d7ec6d090b36e027f505
BryanS1989 <bryanstevenal@gmail.com>
2023-02-08 22:55:56 +0100

    FRONT - call /login at axios.service

--- frontend-app/src/services/axios.service.ts
+++ frontend-app/src/services/axios.service.ts
@@ -5,10 +5,10 @@ import type User from '@/interfaces/users/user.interface';
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:80';
 
-
 const login = (user: User) => {
-    axios.get('/sanctum/csrf-cookie').then((response) => {
+    return axios.get('/sanctum/csrf-cookie').then((response) => {
         console.log(response, user);
+        return axios.post('/login', user);
     });
 };
 
