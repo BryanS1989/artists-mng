b438d35364436c74522612c5d25acd33a9cec42c
BryanS1989 <bryanstevenal@gmail.com>
2023-02-11 17:26:41 +0100

    FRONT - Call getUserFromLocalStorage at App onBeforeMount

--- frontend-app/src/App.vue
+++ frontend-app/src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import { onBeforeMount } from 'vue';
 import { RouterView } from 'vue-router';
 import HeaderMain from '@/components/shared/HeaderMain.vue';
+import { artistsMngStore } from '@/stores/artistsMng.store';
+
+onBeforeMount(() => {
+    console.log('[APP] [onBeforeMount]');
+    artistsMngStore().getUserFromLocalStorage();
+});
 </script>
 
 <template>
