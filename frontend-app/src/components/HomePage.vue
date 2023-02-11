e659befb6efe634015a4a66f4277a6961c4bc395
BryanS1989 <bryanstevenal@gmail.com>
2023-02-11 16:18:41 +0100

    FRONT - Modify HomePage in order to whoe user name

--- frontend-app/src/components/HomePage.vue
+++ frontend-app/src/components/HomePage.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onBeforeMount, computed } from 'vue';
+import { artistsMngStore } from '@/stores/artistsMng.store';
+
+const userName = computed(() => {
+    const store = artistsMngStore();
+    return store.authUser.name;
+});
+
+onBeforeMount(() => {
+    console.log('[HomePage] [onBeforeMount]');
+});
+</script>
 
 <template>
-    <h1>Home Page</h1>
+    <h1>Welcome {{ userName }}</h1>
 </template>
