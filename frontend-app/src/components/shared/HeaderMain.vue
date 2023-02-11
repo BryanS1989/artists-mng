001eb4b8030f62c9723876b73a554aa919ebc958
BryanS1989 <bryanstevenal@gmail.com>
2023-02-11 17:55:08 +0100

    FRONT - Apply display flex row to header

--- frontend-app/src/components/shared/HeaderMain.vue
+++ frontend-app/src/components/shared/HeaderMain.vue
@@ -6,9 +6,19 @@ import { artistsMngStore } from '@/stores/artistsMng.store';
 <template>
     <header>
         <h1>Artists Management</h1>
-        <hr />
         <Navbar v-if="artistsMngStore().isAuthenticated" />
     </header>
+    <hr />
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: flex-start;
+}
+h1 {
+    margin-right: auto;
+}
+</style>
