16b81058302ddc7dc1040de9b89eeaf4e5cfbeb7
BryanS1989 <bryanstevenal@gmail.com>
2023-02-12 15:56:32 +0100

    FRONT - Create ArtistsView

---
+++ frontend-app/src/views/ArtistsView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { artistsMngStore } from '@/stores/artistsMng.store';
+import { onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+    console.log('[ArtistsView] [onBeforeMount]');
+    artistsMngStore().getArtists();
+});
+</script>
+
+<template>
+    <main></main>
+</template>
