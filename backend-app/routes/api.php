f776416808bc55fc8f407ebd281edc77b075b658
BryanS1989 <bryanstevenal@gmail.com>
2023-02-12 13:51:41 +0100

    BACK - Add routes for Artists CRUD

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Artist\DeleteArtistByIdController;
+use App\Http\Controllers\Artist\GetArtistByIdController;
+use App\Http\Controllers\Artist\GetArtistsController;
+use App\Http\Controllers\Artist\PostArtistController;
+use App\Http\Controllers\Artist\UpdateArtistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +22,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
+    });
+
+    Route::controller(ArtistController::class)->group(function () {
+        Route::get('/artist', GetArtistsController::class);
+        Route::get('/artist/{id}', GetArtistByIdController::class);
+        Route::post('/artist', PostArtistController::class);
+        Route::put('/artist/{id}', UpdateArtistController::class);
+        Route::delete('/artist/{id}', DeleteArtistByIdController::class);
     });
 });
 
