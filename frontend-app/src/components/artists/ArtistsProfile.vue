d03695ae5c854476cb0d5c2f9e7eefb9e54e8e4a
BryanS1989 <bryanstevenal@gmail.com>
2023-02-19 21:00:54 +0100

    FRONT - WIP - Create artistsProfile component

---
+++ frontend-app/src/components/artists/ArtistsProfile.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+    console.log('[ArtistProfile] [onBeforeMount()]');
+});
+</script>
+
+<template>
+    <p>ArtistProfile</p>
+</template>
+
+<style scoped></style>
