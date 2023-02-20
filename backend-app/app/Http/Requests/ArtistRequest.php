d6ddab0fb977f8c9f12dea6d8f09a9e651e5d138
BryanS1989 <bryanstevenal@gmail.com>
2023-02-20 20:51:35 +0100

    BACK - Validate POST ArtistsRequest

--- backend-app/app/Http/Requests/ArtistRequest.php
+++ backend-app/app/Http/Requests/ArtistRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\Countries;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use Illuminate\Validation\Rules\File;
 
 class ArtistRequest extends FormRequest
 {
@@ -13,7 +16,8 @@ class ArtistRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        // TODO Check if user is allowed to create artists
+        return true;
     }
 
     /**
@@ -37,7 +41,14 @@ class ArtistRequest extends FormRequest
         if ($this->method() !== 'POST') return [];
 
         return [
-
+            'picture' => ['bail', 'nullable',  File::image()],
+            'code' => 'bail|required|unique:artists,code|max:50',
+            'name' => 'bail|required|max:50',
+            'surname' => 'bail|nullable|max:100',
+            'country' => ['bail', 'required', new Enum(Countries::class)],
+            'email' => 'bail|nullable|email|max:100',
+            'prefix' => 'bail|nullable|string|max:5',
+            'phone' => 'bail|required|string|max:20',
         ];
     }
 
